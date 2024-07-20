cc587dd9c2487321f76d53d40c55418dc5957e81
eloizekondras <kondraseloh@gmail.com>
2024-07-20 10:07:29 -0300

    commit

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let search = document.querySelector('.search-box');
+document.addEventListener("DOMContentLoaded", function() {
+    // Seleciona os elementos necessários
+    const menuIcon = document.getElementById('menu-icon');
+    const navbar = document.querySelector('.navbar');
+    const searchIcon = document.getElementById('search-icon');
+    const searchBox = document.querySelector('.search-box');
 
-    document.querySelector('#search-icon').onclick = () => {
-        search.classList.toggle('active');
-    }
+    // Adiciona o evento de clique para o ícone de menu
+    menuIcon.addEventListener('click', () => {
+        navbar.classList.toggle('active');
+    });
+
+    // Adiciona o evento de clique para o ícone de pesquisa
+    searchIcon.addEventListener('click', () => {
+        searchBox.classList.toggle('active');
+    });
+
+    // Fecha o menu quando um link é clicado
+    document.querySelectorAll('.navbar a').forEach(link => {
+        link.addEventListener('click', () => {
+            navbar.classList.remove('active');
+        });
+    });
 });
-
